1a65294cb9d8121e6daddb74cd78bbffaeaac762
FinianScott <25807449+FinianScott@users.noreply.github.com>
2026-04-10 12:15:34 -0400

    Add css for divs
    Divs had no css and weren't showing up on screen. Add css code to make
    divs and use flexbox to create grid. Update js to build rows as well.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,20 @@
 //FUNCTION FOR BUILDING DIVS. GIVEN NUMBER. For statement up to number squared append div. 
 function buildDivs(num){
-    for (let i = 0; i <= num * num; i++){
+    for (let i = 0; i < num; i++){
         const container = document.querySelector("#container");
-        const squareDiv = document.createElement('div');
-        squareDiv.setAttribute('class', 'gridDiv');
-        console.log(`im div${i}`)
-        container.appendChild(squareDiv);
+        const divRow = document.createElement('div');
+        divRow.setAttribute('class', 'divRow');
+        
+        for (let x = 0; x< num; x++){
+            const squareDiv = document.createElement('div');
+            squareDiv.setAttribute('class', 'gridDiv');
+            console.log(`im div${i}`)
+            divRow.appendChild(squareDiv);
+
+            //eventually add listener for hover to function
+        }
+        container.appendChild(divRow);
     
-        //eventually add listener for hover to function
     }
 }
 
