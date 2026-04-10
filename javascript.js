019740763e2ee641918cdc22069dddc178bd5ff2
FinianScott <25807449+FinianScott@users.noreply.github.com>
2026-04-10 11:40:10 -0400

    Add initial js
    Initial js file had no code. Add code to initialize 16x16 divs.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
 //FUNCTION FOR BUILDING DIVS. GIVEN NUMBER. For statement up to number squared append div. 
+function buildDivs(num){
+    for (let i = 0; i <= num * num; i++){
+        const container = document.querySelector("#container");
+        const squareDiv = document.createElement('div');
+        squareDiv.setAttribute('class', 'gridDiv');
+        console.log(`im div${i}`)
+        container.appendChild(squareDiv);
+    
+        //eventually add listener for hover to function
+    }
+}
 
-//eventually add listener for hover to function
 
-
-
+buildDivs(16);
 //Set number to sixteen, then run function. eventually add listener here to update page if new number entered. 
