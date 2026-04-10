f9ded0a573f1eedbbb564ef73c85bbcc72864589
FinianScott <25807449+FinianScott@users.noreply.github.com>
2026-04-09 20:07:57 -0400

    Add comments to js
    JS file empty. Add comments to plan out approach to implementations.

---
+++ javascript.js
@@ -0,0 +1,7 @@
+//FUNCTION FOR BUILDING DIVS. GIVEN NUMBER. For statement up to number squared append div. 
+
+//eventually add listener for hover to function
+
+
+
+//Set number to sixteen, then run function. eventually add listener here to update page if new number entered. 
