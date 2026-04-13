dc695b041568937e0fe4b815132969ecdf418942
FinianScott <25807449+FinianScott@users.noreply.github.com>
2026-04-13 17:49:28 -0400

    Fix grid issues
    Grid would expand and shrink due to borders on square divs. Remove
    borders and add in colors to set base of etch-a-sketch.

--- javascript.js
+++ javascript.js
@@ -8,8 +8,8 @@ function buildDivs(num){
         for (let x = 0; x< num; x++){
             const squareDiv = document.createElement('div');
             squareDiv.setAttribute('class', 'gridDiv');
-            squareDiv.style.height = 50/num + 'vh';
-            squareDiv.style.width = 50/num + 'vh';
+            squareDiv.style.height = 75/num + 'vh';
+            squareDiv.style.width = 75/num + 'vh';
             divRow.appendChild(squareDiv);
 
             //eventually add listener for hover to function
@@ -20,5 +20,5 @@ function buildDivs(num){
 }
 
 
-buildDivs(80);
+buildDivs(16);
 //Set number to sixteen, then run function. eventually add listener here to update page if new number entered. 
